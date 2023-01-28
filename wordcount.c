ab2c1ef7fb2e328c23cdc7fd7c8d8cb64ccd334e
taehyunnkim <44147889+taehyunnkim@users.noreply.github.com>
2023-01-27 16:33:46 -0800

    Count words and characters

--- wordcount.c
+++ wordcount.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <ctype.h>
 
-#define READBUFSIZE 502
+#define READBUFSIZE 501
 
 void processFile(FILE *fp, int *result);
 void processLine(char *line, int *result);
@@ -22,9 +23,11 @@ int main(int argc, char** argv)
 			continue;
 		}
 
-		int result[3];
+		int result[] = {0, 0, 0};
 		processFile(fin, result);
 		printf("Total Lines = %d\n", result[0]);
+		printf("Total Words = %d\n", result[1]);
+		printf("Total Characters = %d\n", result[2]);
 	}
 
 	return EXIT_SUCCESS;
@@ -40,10 +43,33 @@ void processFile(FILE *fp, int *result)
 	}
 
 	result[0] = linec;
-
 	fclose(fp);
 }
 
-void processLine(char *line, int *result) {
+void processLine(char *c, int *result) {
+	size_t characters = 0;
+	size_t words = 0;
+	
+	// Skip white spaces
+	while (*c && isspace(*c)) {
+		characters++;
+		c++;
+	}
 
+	// Count words
+	while(*c) {
+		words++;
+		while(*c && !isspace(*c)) {
+			characters++;
+			c++;
+		}
+		
+		while(*c && isspace(*c)) {
+			characters++;
+			c++;
+		}
+	}
+
+	result[1] += words;
+	result[2] += characters;
 }
